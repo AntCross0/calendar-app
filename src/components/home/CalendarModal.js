538b8b5600c348019b0470630ccc77001f46c54c
AntCross0 <thedurooo@gmail.com>
2023-06-15 21:20:21 -0400

    update calendar style and init date higher than endDate validation

--- src/components/home/CalendarModal.js
+++ src/components/home/CalendarModal.js
@@ -2,6 +2,8 @@ import moment from 'moment/moment';
 import React, { useState } from 'react';
 import DateTimePicker from 'react-datetime-picker';
 import Modal from 'react-modal';
+import 'react-datetime-picker/dist/DateTimePicker.css';
+import 'react-calendar/dist/Calendar.css';
 
 
 
@@ -19,20 +21,30 @@ const customStyles = {
 Modal.setAppElement('#root');
 
 const now = moment().seconds(0);
+const nowplus1h = now.clone().add(1, 'hours');
 
 const CalendarModal = () => {
 
     let [dateStart, setDateStart] = useState(now.toDate());
+    let [dateEnd, setDateEnd] = useState(nowplus1h.toDate());
 
     const closeModal = () => {
     }
 
-
-
-
     const handleStartDateChange = (e) => {
         console.log(e);
+        setDateStart(e);
+
     }
+
+    const handleEndDateChange = (e) => {
+        console.log(e);
+        setDateEnd(e);
+    }
+
+
+
+
     return (
 
         <div>
@@ -53,21 +65,22 @@ const CalendarModal = () => {
                             autoComplete='off'
                             className='group/title transition-all py-2 pl-4 my-1  mt-3 border-2 p-1 border-black/10 w-full rounded outline-none focus:border-blue-600' ></input>
                         <label className=' font-medium text-medium text-sm ml-2 text-black/60'>Titulo</label>
-                        <input type='text'
 
-                            autoComplete='off'
-                            className='transition-all  my-1 py-2 pl-4 mt-3 border-2 p-1 border-black/10 w-full rounded outline-none  focus:border-blue-600' ></input>
                         <DateTimePicker
                             onChange={handleStartDateChange}
-                            value={now.toDate()}
-                            className=''
+                            value={dateStart}
+                            className='mt-3 my-1'
                         >
                         </DateTimePicker>
-                        <label className='font-medium text-medium text-sm ml-2 text-black/60'>Fecha de inicio</label>
 
-                        <input type='text'
-                            autoComplete='off'
-                            className='transition-all  my-1 py-2 pl-4 mt-3 border-2 p-1 border-black/10 w-full rounded outline-none  focus:border-blue-600' ></input>
+                        <label className='font-medium text-medium text-sm ml-2 text-black/60'>Fecha de inicio</label>
+                        <DateTimePicker
+                            onChange={handleEndDateChange}
+                            value={dateEnd}
+                            minDate={dateStart}
+                            className='mt-3 my-1'
+                        >
+                        </DateTimePicker>
                         <label className='font-medium text-medium text-sm ml-2 text-black/60'>Fecha final</label>
                         <textarea
                             autoComplete='off'
